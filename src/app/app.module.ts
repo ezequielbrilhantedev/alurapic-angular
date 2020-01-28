1b008a7f73562a823db4f92ba01754c8b8cb1eda
ezequielbrilhantedev <ezequielrb7@gmail.com>
2020-01-28 15:28:22 -0300

    uso de diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
 
-import { PhotoComponent } from './photo/photo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
