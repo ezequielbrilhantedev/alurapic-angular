9e8f5e535851ef9781d23e1e860b54b191abfad6
ezequielbrilhantedev <ezequielrb7@gmail.com>
2020-01-29 16:59:07 -0300

    aupdate

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PhotosModule } from './photos/photos.module';
     BrowserModule,
     AppRoutingModule,
     PhotosModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
