29643cfd097d897813696638f256f1bfc992f345
ezequielbrilhantedev <ezequielrb7@gmail.com>
2020-01-29 09:56:50 -0300

    teste

--- web-app/src/app/photos/photos.module.ts
+++ web-app/src/app/photos/photos.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ]
+    declarations: [ 
+        PhotoComponent
+    ],
+    exports: [ 
+        PhotoComponent 
+    ]
 })
 export class PhotosModule {}
